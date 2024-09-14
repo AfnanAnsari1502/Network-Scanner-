b8c4365e3ecbc72b7a460718e08e7f75cbede9d6
Afnan Ansari <ansari.afnan2000@gmail.com>
2024-09-14 11:25:26 +0530

    Add files via upload

---
+++ network_scanner_project.py
@@ -0,0 +1,29 @@
+import argparse
+import nmap
+
+def argument_parser():
+    """Allow user to specify target host and ports."""
+    parser = argparse.ArgumentParser(description="TCP port scanner. Accepts a hostname/IP address and a list of ports to scan. Attempts to identify the service running on a port.")
+    parser.add_argument("-o", "--host", required=True, help="Host IP address")
+    parser.add_argument("-p", "--ports", required=True, help="Comma-separated port list, such as '25,80,8080'")
+
+    return vars(parser.parse_args())
+
+def nmap_scan(host_id, port_num):
+    """Use nmap utility to check host ports for status."""
+    nm_scan = nmap.PortScanner()
+    nm_scan.scan(host_id, port_num)
+    state = nm_scan[host_id]['tcp'][int(port_num)]['state']  # Indicate the type of scan and port number
+    result = f"[*] {host_id} tcp/{port_num} {state}"
+
+    return result
+
+if __name__ == '__main__':
+    try:
+        user_args = argument_parser()
+        host = user_args["host"]
+        ports = user_args["ports"].split(",")  # Make a list from port numbers
+        for port in ports:
+            print(nmap_scan(host, port))
+    except AttributeError:
+        print("Error: Please provide the host and port numbers.")
